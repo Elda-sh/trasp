19f5e1d508a72826292cc3136f052a4c7d8da07a
Sergey Azarkin <hobagos@gmail.com>
2015-09-25 17:17:55 +0300

    Basic views: index, station and train

--- trainsapp/schedule/views.py
+++ trainsapp/schedule/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
 
-# Create your views here.
+from schedule.models import Train, Station, TrainPath
+
+
+def home(request):
+    stations = Station.objects.all()
+    trains = Train.objects.all()
+    return render_to_response('index.html', locals())
+
+
+def view_station(request, station_id):
+    station = get_object_or_404(Station, pk=station_id)
+    trains = TrainPath.objects.filter(station=station)
+    return render_to_response('station.html', locals())
+
+
+def view_train(request, train_id):
+    train = get_object_or_404(Train, pk=train_id)
+    path = TrainPath.objects.filter(train=train)
+    return render_to_response('train.html', locals())
--- trainsapp/trainsapp/urls.py
+++ trainsapp/trainsapp/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from schedule import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home),
+    url(r'^station/(?P<station_id>[0-9]+)$', views.view_station,
+        name='station'),
+    url(r'^train/(?P<train_id>[0-9]+)$', views.view_train,
+        name='train'),
 ]
