87607435da5d3ff732f5eac2c1bca9df9cc7e110
Sergey Azarkin <hobagos@gmail.com>
2015-09-25 17:17:08 +0300

    Added base template and fix settings

--- trainsapp/trainsapp/settings.py
+++ trainsapp/trainsapp/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'trainsapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,3 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/s/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+if DEBUG:
+    INSTALLED_APPS += ('debug_toolbar',)
