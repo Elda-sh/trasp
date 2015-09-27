c6bb495b8d350a5d4d8076cd8ecaec76149ece45
Sergey Azarkin <hobagos@gmail.com>
2015-09-27 15:57:41 +0300

    New station page: calendar, limit list to current mounth

--- trainsapp/schedule/views.py
+++ trainsapp/schedule/views.py
@@ -1,3 +1,4 @@
+from itertools import groupby
 import json
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
@@ -5,6 +6,29 @@ from django.utils import dateparse
 from django.utils import timezone
 
 from schedule.models import City, Train, Station, TrainPath
+
+
+def create_calendar(start, now, events, days):
+    calendar = []
+
+    for j in xrange(4):
+        week = []
+        for i in xrange(7):
+            day = {}
+            day['date'] = start + timezone.timedelta(days=i)
+            day['is_today'] = (now.date() == day['date'])
+            try:
+                i = days.index(day['date'])
+                day['events'] = events[i]
+            except ValueError:
+                day['events'] = []
+
+            week.append(day)
+
+        calendar.append(week)
+        start = week[-1]['date'] + timezone.timedelta(days=1)
+
+    return calendar
 
 
 def home(request):
@@ -31,8 +55,23 @@ def home(request):
 
 
 def view_station(request, station_id):
+    now = timezone.localtime(timezone.now())
+    start = now.date() - timezone.timedelta(days=now.weekday())
+
     station = get_object_or_404(Station, pk=station_id)
-    trains = TrainPath.objects.filter(station=station)
+    trains = TrainPath.objects.filter(station=station).filter(
+        time__gte=start,
+        time__lte=start + timezone.timedelta(days=28)
+    )
+
+    trains_by_day = []
+    days = []
+    for k, g in groupby(trains, lambda e: e.time.date()):
+        trains_by_day.append(list(g))
+        days.append(k)
+
+    calendar = create_calendar(start, now, trains_by_day, days)
+
     return render_to_response('station.html', locals())
 
 
